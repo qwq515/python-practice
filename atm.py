252f0d4b5ea5e0d9e1c2b858f1adc03d4e4def5b
uranus_0831 <uranus_0831@qq.com>
2026-09-17 12:40:08 +0000

    继续写bug

--- atm.py
+++ atm.py
@@ -1,12 +1,14 @@
 balance = 1000
-def deposit(a,b):
-    c = a+b
-    return(c)
-def withdraw(a,b):
-        c = b - a
-        return(c)
-def show_balance(a):
-    return(a)
+#def valueerror(mon)
+
+def deposit(deposit,balance):
+    rest = deposit+balance
+    return(rest)
+def withdraw(withdraw,balance):
+        rest = balance - withdraw
+        return(rest)
+def show_balance(mon):
+    return(mon)
 
 while True:
     order = input("请输入指令：")
@@ -14,19 +16,35 @@ while True:
         print("退出")    
         break
     elif order == "d":
-        dep = int(input("存入金额："))
-        balance = deposit(balance,dep)
-        print("存款成功")
-        continue
-    elif order == "w":
-        wid = int(input("取款金额："))
-        if wid > balance:
-            print("取款金额不能大于存款金额")
+        dep = input("存入金额：")
+        try:
+            dep = int(dep)
+            if dep <= 0:
+                print("无效存款")
+            else:
+                balance = deposit(balance,dep)
+                print("存款成功")
+                continue
+        except ValueError:
+            print("无效输入")
             continue
-        else:
-            balance = withdraw(wid,balance)
-            print("取款成功")
-        continue
+    elif order == "w":
+        wid = input("取款金额：")
+        try:
+            wid = int(wid)
+            if wid <= 0:
+                print("无效输入")
+            else:
+                if wid > balance:
+                    print("取款金额不能大于存款金额")
+                    continue
+                else:
+                    balance = withdraw(wid,balance)
+                    print("取款成功")
+                continue
+        except ValueError:
+            print("无效输入")
+            continue
     elif order == "s":
         sh = show_balance(balance)
         print("当前金额：",sh)
