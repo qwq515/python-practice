c16684f647ba358cd45335035a7ed63364dd31c0
uranus_0831 <uranus_0831@qq.com>
2026-09-17 09:59:44 +0000

    写了更多的bug

--- atm.py
+++ atm.py
@@ -1,26 +1,31 @@
-order = input("请输入指令：")
 balance = 1000
 def deposit(a,b):
     c = a+b
     return(c)
 def withdraw(a,b):
-    if a > b:
-        print("取出的钱不能大于本金哦")
-    else:
         c = b - a
         return(c)
 def show_balance(a):
-    reutrn(a)
-while order != "q":
-    if order == "d":
+    return(a)
+
+while True:
+    order = input("请输入指令：")
+    if order == "q":
+        print("退出")    
+        break
+    elif order == "d":
         dep = int(input("存入金额："))
         balance = deposit(balance,dep)
         print("存款成功")
         continue
     elif order == "w":
         wid = int(input("取款金额："))
-        balance = withdraw(balance,wid)
-        print("取款成功")
+        if wid > balance:
+            print("取款金额不能大于存款金额")
+            continue
+        else:
+            balance = withdraw(wid,balance)
+            print("取款成功")
         continue
     elif order == "s":
         sh = show_balance(balance)
