9cc6038061b2b96379b22c9ced7544b8111d23d3
uranus_0831 <uranus_0831@qq.com>
2026-09-18 16:15:58 +0000

    增加了同级错误返回

--- roster.py
+++ roster.py
@@ -16,31 +16,37 @@ while True:
         break
     elif order =="add":
         inf = {"name":"","age":0,"grade":0.0}
+        
         name_add = input("请输入姓名:")
         inf["name"] = name_add
         
-        age_add = input("请输入年龄:")
-        age_add = check(int, age_add)
-        if age_add is None:
-            print("年龄格式错误")
-            continue        #我们期望的逻辑是在输入年龄错误后能再次输入年龄，而不是从0开始再次输入这个人的完整信息，但不知道怎么实现
-        elif  0 < age_add < 100:   
-            inf["age"] = age_add
-        else:
-            print("无效年龄")   #一样，不知道怎么重新输入年龄
-            continue
-        grade_add = input("请输入成绩:")
-        grade_add = check(float,grade_add)
-        if grade_add is None:
-            print("成绩格式错误")
-            continue
-        elif 0 <= grade_add <= 100:     #python支持连续比较
-            inf["grade"] = grade_add            
-        else:
-            print("无效成绩")
-            continue
-        lst.append(inf)
-        continue        
+        while True:
+            age_add = input("请输入年龄:")
+            age_add = check(int, age_add)
+            if age_add is None:
+                print("年龄格式错误")
+                continue        #我们期望的逻辑是在输入年龄错误后能再次输入年龄，而不是从0开始再次输入这个人的完整信息，但不知道怎么实现
+            elif  0 < age_add < 100:   
+                inf["age"] = age_add
+                break
+            else:
+                print("无效年龄")   #一样，不知道怎么重新输入年龄
+                continue
+        
+        while True:
+            grade_add = input("请输入成绩:")
+            grade_add = check(float,grade_add)
+            if grade_add is None:
+                print("成绩格式错误")
+                continue
+            elif 0 <= grade_add <= 100:     #python支持连续比较
+                inf["grade"] = grade_add  
+                break          
+            else:
+                print("无效成绩")
+                continue
+            lst.append(inf)
+            continue        
     elif order == "show":
         for student in lst:
             print(
@@ -60,7 +66,7 @@ while True:
                 break
         if not found:
             print("找不到")
-        
+
     elif order == "del":
         d_name = input("请输入姓名")
         found = None
@@ -72,4 +78,5 @@ while True:
                 break
         if not found:
             print("找不到")
+
     
