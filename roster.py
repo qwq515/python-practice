da0c5fd3a885e2fb386e5e3a8165a72310b60553
uranus_0831 <uranus_0831@qq.com>
2026-09-19 03:25:34 +0000

    Two check functions have been created,allowing queries with same name

--- roster.py
+++ roster.py
@@ -7,12 +7,43 @@ def check(value,add):
         return(add)
     except ValueError:
         return(None)
-    
+
+def check_age():
+    while True:
+        age_add = input("Please enter age:")
+        age_add = check(int, age_add)
+        if not age_add:
+            print("Incorrect input")
+            continue
+        elif 0 <= age_add <= 100:
+            return(age_add)
+            break
+        else:
+            print("Incorrect number")
+            continue
+def check_grade():
+    while True:
+        grade_add = input("Please enter grade:")
+        grade_add = check(float, grade_add)
+        if not grade_add:
+            print("Incorrect input")
+            continue
+        elif 0 < grade_add < 100:
+            return(grade_add)
+            break
+        else:
+            print("Incorrect number")
+            continue
 while True:
     order = input("请输入命令：")
     
     if order == "quit":
-        print(lst)
+        for student in lst:
+            print(
+                student["name"],
+                student["age"],"岁",
+                student["grade"],"分"
+            )                
         break
     elif order =="add":
         inf = {"name":"","age":0,"grade":0.0}
@@ -20,12 +51,12 @@ while True:
         name_add = input("请输入姓名:")
         inf["name"] = name_add
         
-        while True:
+        '''while True:
             age_add = input("请输入年龄:")
             age_add = check(int, age_add)
             if age_add is None:
                 print("年龄格式错误")
-                continue        #我们期望的逻辑是在输入年龄错误后能再次输入年龄，而不是从0开始再次输入这个人的完整信息，但不知道怎么实现
+                continue       
             elif  0 < age_add < 100:   
                 inf["age"] = age_add
                 break
@@ -44,9 +75,11 @@ while True:
                 break          
             else:
                 print("无效成绩")
-                continue
-            lst.append(inf)
-            continue        
+                continue'''
+        inf["age"] = check_age()
+        inf["grade"] = check_grade()   
+        lst.append(inf)
+        continue        
     elif order == "show":
         for student in lst:
             print(
@@ -63,7 +96,6 @@ while True:
                 print("姓名:", found["name"])
                 print("年龄:", found["age"])
                 print("成绩:", found["grade"])   
-                break
         if not found:
             print("找不到")
 
