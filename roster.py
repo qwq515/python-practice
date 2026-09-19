5248441b2c70f4d52da55056e0ff851e3556efa6
uranus_0831 <uranus_0831@qq.com>
2026-09-19 13:45:03 +0000

    maybe the last debug

--- roster.py
+++ roster.py
@@ -1,20 +1,21 @@
-#困困困
 import json
+
 def save_data(lst):
     with open("roster_student.json","w",encoding="utf-8")as f:
         json.dump(lst, f,ensure_ascii=False,indent=2)
 
-try:
-    with open("roster_student.json","r",encoding="utf-8")as f:
-        lst = json.load(f)
-except FileNotFoundError:
-    lst = []
-    save_data(lst)
-except json.JSONDecodeError:
-    print("The JSON file is corrupted;please check the file")
-    exit()
-
-
+def load_data():
+    try:
+        with open("roster_student.json","r",encoding="utf-8")as f:
+            lst = json.load(f)
+            return(lst)
+    except FileNotFoundError:
+        lst = []
+        save_data(lst)
+        return(lst)
+    except json.JSONDecodeError:
+        print("The JSON file is corrupted;please check the file")
+        exit()
 
 def check(value,add):
     try:
@@ -35,6 +36,20 @@ def check_age_grade(var,data_type,num_min,num_max,left_equal,right_equal):
         else:
             print("Incorrect number")
             continue
+
+def add_student(lst):
+    inf = {"name":"","age":0,"grade":0.0}
+        
+    name_add = input("请输入姓名:")
+    inf["name"] = name_add
+    inf["age"] = check_age_grade("age",int,0,100,False,False)
+    inf["grade"] = check_age_grade("grade",float,0,100,True,True)   
+    lst.append(inf)
+    save_data(lst)        
+    return(lst)
+
+lst = load_data()
+
 while True:
     order = input("请输入命令：")
     
@@ -49,18 +64,9 @@ while True:
         break
 
     elif order =="add":
-        inf = {"name":"","age":0,"grade":0.0}
-        
-        name_add = input("请输入姓名:")
-        inf["name"] = name_add
-        
-        #var_age = "age"
-        #var_grade = "grade"
-        inf["age"] = check_age_grade("age",int,0,100,False,False)
-        inf["grade"] = check_age_grade("grade",float,0,100,True,True)   
-        lst.append(inf)
-        save_data(lst)
-        continue        
+        lst = add_student(lst)
+        continue
+
     elif order == "show":
         for student in lst:
             print(
@@ -92,5 +98,17 @@ while True:
                 break
         if not found:
             print("找不到")
+    elif order == "update":
+        re_name = input("enter name")
+        for stu in lst:
+            found = None
+            if re_name == stu["name"]:
+                found = stu
+                stu["name"] = input("new name:")
+                stu["age"] = input("new age")
+                stu["grade"] = input("new grade")
+                save_data(lst)
+        if found is None:
+                print("404 not find")
 
     
