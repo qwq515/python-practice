a644b3d699d57daab87c26133e5daad31096642f
uranus_0831 <uranus_0831@qq.com>
2026-09-20 13:12:41 +0000

    v1.1

--- text_counter.py
+++ text_counter.py
@@ -3,14 +3,16 @@ word_split =text.split() #word_split is a list,however, it cannot identify symno
 
 def times_max(lst):
     t_max = 0
-    for data in lst:
-        if data["times"] > t_max:
-            t_max = data["times"]
-            max_data = data
-            return(max_data)
-    return(None)
+    if not lst:
+        return(None)
+    else:
+        for data in lst:
+            if data["count"] > t_max:
+                t_max = data["count"]
+                max_data = data
+        return(max_data)
 
-lst = [{"seen":"","times":0}]
+lst = []
 num = 0
 not_num = 0
 text = text.replace(" ", "")
@@ -19,12 +21,12 @@ for word in word_split:
     num += 1
     found = None
     for dic in lst:
-        if word == dic["seen"]:
-            dic["times"] += 1
+        if word == dic["word"]:
+            dic["count"] += 1
             found = word
             break
     if found is None:
-        count_dic = {"seen":word,"times":1}
+        count_dic = {"word":word,"count":1}
         lst.append(count_dic)
         not_num += 1
 max_word =times_max(lst)
@@ -34,5 +36,5 @@ if max_word is None:
 else:
     print("character count:",t_len)
     print("word count",num)
-    print("not repeated character count:",not_num)
+    print("unique word count:",not_num)
     print("the most frequently mentioned word:",max_word)
