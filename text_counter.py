8d3def6464d734a61717713de06e0a73745704c6
uranus_0831 <uranus_0831@qq.com>
2026-09-20 13:02:03 +0000

    fixed the issue with empty lists

--- text_counter.py
+++ text_counter.py
@@ -7,8 +7,8 @@ def times_max(lst):
         if data["times"] > t_max:
             t_max = data["times"]
             max_data = data
-    return(max_data)
-
+            return(max_data)
+    return(None)
 
 lst = [{"seen":"","times":0}]
 num = 0
@@ -17,8 +17,8 @@ text = text.replace(" ", "")
 t_len = len(text)
 for word in word_split:
     num += 1
+    found = None
     for dic in lst:
-        found = None
         if word == dic["seen"]:
             dic["times"] += 1
             found = word
@@ -28,8 +28,11 @@ for word in word_split:
         lst.append(count_dic)
         not_num += 1
 max_word =times_max(lst)
-
-print("character count:",t_len)
-print("word count",num)
-print("not repeated character count:",not_num)
-print("the most frequently mentioned word:",max_word)
+if max_word is None:
+    print("please enter the words")
+    
+else:
+    print("character count:",t_len)
+    print("word count",num)
+    print("not repeated character count:",not_num)
+    print("the most frequently mentioned word:",max_word)
