367dc97b8b339f345332d6404fc8da8435560126
uranus_0831 <uranus_0831@qq.com>
2026-09-23 15:23:52 +0000

    gre

--- to_do_list.py
+++ to_do_list.py
@@ -5,40 +5,43 @@
 #functional functions
 def done(todo_lst):
     found_item = input("please enter your goal item:")
-    found = None
-    for things in todo_lst:
+    found1 = None
+    for num,things in todo_lst.items():
         if found_item == things["items"]:
-            while True:  
-                print("you want to change:",things)
-                judgment =input("y/n:")
-                if judgment == "y":
-                    things["completed"] = True
-                    found = 1
-                    break
-                elif judgment == "n":
-                    break
-                else:
-                    print("invalid input")
-                    continue
-    if found == None:
+            print(things["items"],"| finish time:",things["finish time"],num)   
+            found1 = 1    
+    if found1 is None:
         print("cannot find this item")
+    while True:
+        del_num = input("please enter your goal number:")
+        for num,things in todo_lst.items():
+            if del_num == num and found_item == things:
+                todo_lst[num]["completed"] = True
+
+                break
+        break
+        
+
+            
 
 def show(todo_lst):
-    for things in todo_lst:
+    for num,things in todo_lst.items():
         if things["completed"] == False:
             print("[ ]",things["items"],"| finish time:",things["finish time"])
         if things["completed"] == True:
             print("[✓]",things["items"],"| finish time:",things["finish time"])
 
 #main program
-lst = []
+lst = {}
+num = 0
 while True:
     order = input("please enter your order:")
     if order == "add":
-        todo_dic = {"items":"","finish time":"","completed":False}
-        todo_dic["items"] = input("plsase enter your item:")
-        todo_dic["finish time"] = input("finish time:")
-        lst.append(todo_dic)
+        num += 1
+        todo_dic = {num:{"items":"","finish time":"","completed":False}}
+        todo_dic[num]["items"] = input("plsase enter your item:")
+        todo_dic[num]["finish time"] = input("finish time:")
+        lst.update(todo_dic)
     elif order == "show":
         show(lst)
     elif order == "done":
