90754a94e02cb357229bbf2fd3ecde8b63eb1c5b
uranus_0831 <uranus_0831@qq.com>
2026-09-23 13:53:43 +0000

    beautify show

--- study.py
+++ study.py
@@ -1,8 +1,11 @@
 '''lst = ["sj","wiev","sjsb"]
 len_lst = len(lst)
-print(len_lst)'''
+print(len_lst)
 def test():
     return(1,2)
 a,b = test()
 print(a)
-print(b)
+print(b)'''
+a =1
+b =2
+print(a,b)
--- to_do_list.py
+++ to_do_list.py
@@ -1,3 +1,7 @@
+#import json
+
+#utility functions
+
 #functional functions
 def done(todo_lst):
     found_item = input("please enter your goal item:")
@@ -6,7 +10,7 @@ def done(todo_lst):
         if found_item == things["items"]:
             while True:  
                 print("you want to change:",things)
-                judgment =input("y/n")
+                judgment =input("y/n:")
                 if judgment == "y":
                     things["completed"] = True
                     found = 1
@@ -18,21 +22,30 @@ def done(todo_lst):
                     continue
     if found == None:
         print("cannot find this item")
-    return(todo_lst)
+
+def show(todo_lst):
+    for things in todo_lst:
+        if things["completed"] == False:
+            print("[ ]",things["items"],"| finish time:",things["finish time"])
+        if things["completed"] == True:
+            print("[✓]",things["items"],"| finish time:",things["finish time"])
 
 #main program
 lst = []
 while True:
     order = input("please enter your order:")
-    todo_dic = {"items":"","finish time":"","completed":False}
     if order == "add":
+        todo_dic = {"items":"","finish time":"","completed":False}
         todo_dic["items"] = input("plsase enter your item:")
         todo_dic["finish time"] = input("finish time:")
         lst.append(todo_dic)
-        continue
-    if order == "show":
-        print(lst)
-    if order == "done":
-        lst = done(lst)
-    if order == "quit":
+    elif order == "show":
+        show(lst)
+    elif order == "done":
+        done(lst)
+    elif order == "quit":
         break
+    else:
+        print("wrong input")
+        continue
+
