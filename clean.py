957e020453a15bb81849193400b7e95a4dffc218
qwq515 <uranus_0831@qq.com>
2026-09-26 09:02:30 +0800

    v1

---
+++ clean.py
@@ -0,0 +1,23 @@
+import string
+
+def clean(sen):
+    result = []
+    word = ""
+    for char in sen:
+        if char not in string.punctuation:
+            if char != " ":
+                word += char
+            elif char == " ":
+                result.append(word)
+                word = ""
+        else:
+            continue
+    if word != "":
+        result.append(word)
+    return result
+sentence = input("please enter your characters")
+result = clean(sentence)
+breakpoint()
+result_cl = set(result)
+breakpoint()
+print(result_cl)
